7b250325966d7e9a47ba2651bfc40a76b632446b
Ferdinand Polpol <ferdinand.polpol@chromedia.com>
2023-09-19 18:00:03 +0800

    devops changes

--- web/web/settings.py
+++ web/web/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': os.environ.get("POSTGRES_DB", 'postgres'),
+        'USER': os.environ.get("POSTGRES_USER", 'postgres'),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),
-        'HOST': 'db',
+        'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),
         'PORT': 5432,
     }
 }
