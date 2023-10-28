63037af3e07de922bb0a20c9547d19b5296bdcf6
Ferdinand Polpol <ferdinand.polpol@chromedia.com>
2023-10-28 10:00:44 +0800

    sales admin will allow filter

--- web/web/admin.py
+++ web/web/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from rangefilter.filter import DateRangeFilter
 
 from .models import (
     CustomerType, Customer, Ingredient, Product, ProductionIngredients,
     SupplyItem, Supply, Recipe,
     Production, Sales, Purchase, Supplier
 )
+
 
 
 class IngredientInline(admin.TabularInline):
@@ -22,6 +24,14 @@ class SupplierAdmin(admin.ModelAdmin):
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Purchase._meta.get_fields()]
 
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'product', 'actual_sale', 'date']
+
+    # add filter sidebar for customer and product and date
+    # date should be datepicker
+    list_filter = ['customer', 'product', ('date', DateRangeFilter)]
+
+    
 
 
 admin.site.register(CustomerType)
@@ -33,6 +43,6 @@ admin.site.register(Ingredient)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(ProductionIngredients)
 admin.site.register(Production)
-admin.site.register(Sales)
+admin.site.register(Sales, SalesAdmin)
 admin.site.register(Purchase, PurchaseAdmin)
 admin.site.register(Supplier, SupplierAdmin)
