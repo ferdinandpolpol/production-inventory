3bf8e558a9f2eb0f282d9cc117808a3dfb5302ea
Ferdinand Polpol <ferdinand.polpol@chromedia.com>
2023-10-28 10:11:41 +0800

    more admin updates

--- web/web/admin.py
+++ web/web/admin.py
@@ -17,12 +17,11 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [
         IngredientInline,
     ]
-
     
 class SupplierAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Supplier._meta.get_fields()]
-class PurchaseAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Purchase._meta.get_fields()]
+
+
 
 class SalesAdmin(admin.ModelAdmin):
     list_display = ['customer', 'product', 'actual_sale', 'date']
@@ -31,6 +30,22 @@ class SalesAdmin(admin.ModelAdmin):
     # date should be datepicker
     list_filter = ['customer', 'product', ('date', DateRangeFilter)]
 
+class SupplyAdmin(admin.ModelAdmin):
+    list_display = ['item', 'quantity', 'supplied_at']
+    list_filter = ['item', ('supplied_at', DateRangeFilter)]
+
+class SupplyInline(admin.TabularInline):
+    model = Supply
+
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['supplier', 'notes', 'amount', 'date']
+
+    list_filter = ['supplier', ('date', DateRangeFilter)]
+    inlines = [ 
+        SupplyInline,
+    ]
     
 
 
